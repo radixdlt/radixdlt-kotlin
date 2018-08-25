a7763cb7c82c2a210cdffe0d10a96600148ffeb3
Marc Rubio <marc@radixdlt.com>
2018-08-25 10:54:57 +0100

    Add Documentation and another method in Radix Wallet

---
+++ radixdlt-kotlin/src/test/kotlin/com/radixdlt/client/dapps/wallet/RadixWalletTest.kt
@@ -0,0 +1,25 @@
+package com.radixdlt.client.dapps.wallet
+
+class RadixWalletTest {
+
+    // TODO: Remove when more mature
+    // Null tests not needed in Kotlin since null has to be explicitly allowed
+    // and hence passing null as an argument wont compile
+//    @Test
+//    fun nullTest() {
+//        val api = mock(RadixApplicationAPI::class.java)
+//        val radixWallet = RadixWallet(api)
+//        assertThatThrownBy { radixWallet.getXRDSubUnitBalance(null) }
+//            .isInstanceOf(NullPointerException::class.java)
+//        assertThatThrownBy { radixWallet.getXRDTransactions(null) }
+//            .isInstanceOf(NullPointerException::class.java)
+//        assertThatThrownBy { radixWallet.transferXRD(1, null) }
+//            .isInstanceOf(NullPointerException::class.java)
+//        assertThatThrownBy { radixWallet.transferXRD(1, null, "hi") }
+//            .isInstanceOf(NullPointerException::class.java)
+//        assertThatThrownBy { radixWallet.transferXRDWhenAvailable(1, null) }
+//            .isInstanceOf(NullPointerException::class.java)
+//        assertThatThrownBy { radixWallet.transferXRDWhenAvailable(1, null, "hi") }
+//            .isInstanceOf(NullPointerException::class.java)
+//    }
+}
