4b7a47535a29a9f10b9a43f8d3b51afe0bfb381f
Marc Rubio <marc@radixdlt.com>
2018-10-08 11:28:51 +0100

    Use better err messages in RPC/WS clients

--- radixdlt-kotlin/src/main/kotlin/com/radixdlt/client/core/network/RadixJsonRpcClient.kt
+++ radixdlt-kotlin/src/main/kotlin/com/radixdlt/client/core/network/RadixJsonRpcClient.kt
@@ -143,8 +143,8 @@ class RadixJsonRpcClient(
                                 RuntimeException("Received bad json rpc message: " + received.toString())
                             )
                         }
-                    }, { _ ->
-                        emitter.onError(RuntimeException("Lost connection."))
+                    }, { err ->
+                        emitter.onError(RuntimeException(err.message))
                     })
             }
         )
