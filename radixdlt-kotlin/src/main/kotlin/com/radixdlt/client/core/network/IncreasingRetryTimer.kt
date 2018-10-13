ff7faa266decd810e0a8540008e756c9c6327234
Marc Rubio <marc@radixdlt.com>
2018-10-13 12:11:56 +0100

    Better error logging on RetryTimer

--- radixdlt-kotlin/src/main/kotlin/com/radixdlt/client/core/network/IncreasingRetryTimer.kt
+++ radixdlt-kotlin/src/main/kotlin/com/radixdlt/client/core/network/IncreasingRetryTimer.kt
@@ -9,12 +9,15 @@ import java.util.concurrent.TimeUnit
 
 class IncreasingRetryTimer : Function<Observable<Throwable>, ObservableSource<Long>> {
 
-    private val LOGGER = LoggerFactory.getLogger(IncreasingRetryTimer::class.java)
-
     override fun apply(attempts: Observable<Throwable>): ObservableSource<Long> {
-        return attempts.zipWith(Observable.range(1, 300)) { _, i -> i }
+        return attempts.doOnNext { t -> LOGGER.info(t.toString()) }
+            .zipWith(Observable.range(1, 300)) { _, i -> i }
             .map { i -> Math.min(i * i, 100) }
-            .doOnNext { i -> LOGGER.info("Connection lost. Retrying in $i seconds...") }
+            .doOnNext { i -> LOGGER.info("Retrying in $i seconds...") }
             .flatMap { i -> Observable.timer(i.toLong(), TimeUnit.SECONDS) }
+    }
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(IncreasingRetryTimer::class.java)
     }
 }
